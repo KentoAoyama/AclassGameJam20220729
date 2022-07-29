fc615af32154128231b4ecd2eda954c4437e5831
kent\vantan <atoshi1129@gmail.com>
2022-07-29 17:21:12 +0900

    タイトル

---
+++ Assets/Sawada/TitleManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] Button _startButton;
+    [SerializeField] Button _sousaButton;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+        }
+    }
+}
